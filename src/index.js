2b336b058e12191f805b7081bd4aa8adf59d1528
unknown <shjemhurov@gmail.com>
2023-02-27 15:58:28 +0500

    main position changed to absolute

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import reportWebVitals from "./reportWebVitals";
 import App from "./App";
+// import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 
